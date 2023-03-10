dd100cb54d15c05014982abb8e2a74d3a3b2d9a5
Paula Gray-Overtoom <pgrayove@monroe.lib.in.us>
2023-03-10 11:15:11 -0500

    Results of coder upgrade.

--- js/hide_submit.js
+++ js/hide_submit.js
@@ -2,7 +2,7 @@
  * @file
  * A JavaScript file for the theme.
  * This file should be used as a template for your other js files.
- * It defines a drupal behavior the "Drupal way".
+ * It defines a backdrop behavior the "Backdrop way".
  *
  */
 
@@ -11,18 +11,18 @@
 // - https://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 
-(function ($, Drupal, window, document, undefined) {
+(function ($, Backdrop, window, document, undefined) {
   'use strict';
 
   // To understand behaviors, see https://drupal.org/node/756722#behaviors
-  Drupal.behaviors.hideSubmitBlockit = {
+  Backdrop.behaviors.hideSubmitBlockit = {
     attach: function(context) {
       var timeoutId = null;
       $('form', context).once('hideSubmitButton', function () {
         var $form = $(this);
 
         // Bind to input elements.
-        if (Drupal.settings.hide_submit.hide_submit_method === 'indicator') {
+        if (Backdrop.settings.hide_submit.hide_submit_method === 'indicator') {
           // Replace input elements with buttons.
           $('input.form-submit', $form).each(function(index, el) {
             var attrs = {};
@@ -38,13 +38,13 @@
           // Add needed attributes to the submit buttons.
           $('button.form-submit', $form).each(function(index, el) {
             $(this).addClass('ladda-button button').attr({
-              'data-style': Drupal.settings.hide_submit.hide_submit_indicator_style,
-              'data-spinner-color': Drupal.settings.hide_submit.hide_submit_spinner_color,
-              'data-spinner-lines': Drupal.settings.hide_submit.hide_submit_spinner_lines
+              'data-style': Backdrop.settings.hide_submit.hide_submit_indicator_style,
+              'data-spinner-color': Backdrop.settings.hide_submit.hide_submit_spinner_color,
+              'data-spinner-lines': Backdrop.settings.hide_submit.hide_submit_spinner_lines
             });
           });
           Ladda.bind('.ladda-button', $form, {
-            timeout: Drupal.settings.hide_submit.hide_submit_reset_time
+            timeout: Backdrop.settings.hide_submit.hide_submit_reset_time
           });
         }
         else {
@@ -59,37 +59,37 @@
         $('form', context).submit(function (e) {
           var $inp;
           if (!e.isPropagationStopped()) {
-            if (Drupal.settings.hide_submit.hide_submit_method === 'disable') {
+            if (Backdrop.settings.hide_submit.hide_submit_method === 'disable') {
               $('input.form-submit, button.form-submit', $form).attr('disabled', 'disabled').each(function (i) {
                 var $button = $(this);
-                if (Drupal.settings.hide_submit.hide_submit_css) {
-                  $button.addClass(Drupal.settings.hide_submit.hide_submit_css);
+                if (Backdrop.settings.hide_submit.hide_submit_css) {
+                  $button.addClass(Backdrop.settings.hide_submit.hide_submit_css);
                 }
-                if (Drupal.settings.hide_submit.hide_submit_abtext) {
-                  $button.val($button.val() + ' ' + Drupal.settings.hide_submit.hide_submit_abtext);
+                if (Backdrop.settings.hide_submit.hide_submit_abtext) {
+                  $button.val($button.val() + ' ' + Backdrop.settings.hide_submit.hide_submit_abtext);
                 }
                 $inp = $button;
               });
 
-              if ($inp && Drupal.settings.hide_submit.hide_submit_atext) {
-                $inp.after('<span class="hide-submit-text">' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_atext) + '</span>');
+              if ($inp && Backdrop.settings.hide_submit.hide_submit_atext) {
+                $inp.after('<span class="hide-submit-text">' + Backdrop.checkPlain(Backdrop.settings.hide_submit.hide_submit_atext) + '</span>');
               }
             }
-            else if (Drupal.settings.hide_submit.hide_submit_method !== 'indicator'){
-              var pdiv = '<div class="hide-submit-text' + (Drupal.settings.hide_submit.hide_submit_hide_css ? ' ' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_hide_css) + '"' : '') + '>' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_hide_text) + '</div>';
-              if (Drupal.settings.hide_submit.hide_submit_hide_fx) {
-                $('input.form-submit, button.form-submit', $form).addClass(Drupal.settings.hide_submit.hide_submit_css).fadeOut(100).eq(0).after(pdiv);
+            else if (Backdrop.settings.hide_submit.hide_submit_method !== 'indicator'){
+              var pdiv = '<div class="hide-submit-text' + (Backdrop.settings.hide_submit.hide_submit_hide_css ? ' ' + Backdrop.checkPlain(Backdrop.settings.hide_submit.hide_submit_hide_css) + '"' : '') + '>' + Backdrop.checkPlain(Backdrop.settings.hide_submit.hide_submit_hide_text) + '</div>';
+              if (Backdrop.settings.hide_submit.hide_submit_hide_fx) {
+                $('input.form-submit, button.form-submit', $form).addClass(Backdrop.settings.hide_submit.hide_submit_css).fadeOut(100).eq(0).after(pdiv);
                 $('input.form-submit, button.form-submit', $form).next().fadeIn(100);
               }
               else {
-                $('input.form-submit, button.form-submit', $form).addClass(Drupal.settings.hide_submit.hide_submit_css).hide().eq(0).after(pdiv);
+                $('input.form-submit, button.form-submit', $form).addClass(Backdrop.settings.hide_submit.hide_submit_css).hide().eq(0).after(pdiv);
               }
             }
             // Add a timeout to reset the buttons (if needed).
-            if (Drupal.settings.hide_submit.hide_submit_reset_time) {
+            if (Backdrop.settings.hide_submit.hide_submit_reset_time) {
               timeoutId = window.setTimeout(function() {
                 hideSubmitResetButtons(null, $form);
-              }, Drupal.settings.hide_submit.hide_submit_reset_time);
+              }, Backdrop.settings.hide_submit.hide_submit_reset_time);
             }
           }
           return true;
@@ -106,11 +106,11 @@
         // Clear timer.
         window.clearTimeout(timeoutId);
         timeoutId = null;
-        switch (Drupal.settings.hide_submit.hide_submit_method) {
+        switch (Backdrop.settings.hide_submit.hide_submit_method) {
           case 'disable':
-            $('input.' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_css) + ', button.' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_css), form)
+            $('input.' + Backdrop.checkPlain(Backdrop.settings.hide_submit.hide_submit_css) + ', button.' + Backdrop.checkPlain(Backdrop.settings.hide_submit.hide_submit_css), form)
               .each(function (i, el) {
-                $(el).removeClass(Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_hide_css))
+                $(el).removeClass(Backdrop.checkPlain(Backdrop.settings.hide_submit.hide_submit_hide_css))
                   .removeAttr('disabled');
               });
             $('.hide-submit-text', form).remove();
@@ -121,10 +121,10 @@
             break;
 
           default:
-            $('input.' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_css) + ', button.' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_css), form)
+            $('input.' + Backdrop.checkPlain(Backdrop.settings.hide_submit.hide_submit_css) + ', button.' + Backdrop.checkPlain(Backdrop.settings.hide_submit.hide_submit_css), form)
               .each(function (i, el) {
                 $(el).stop()
-                  .removeClass(Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_hide_css))
+                  .removeClass(Backdrop.checkPlain(Backdrop.settings.hide_submit.hide_submit_hide_css))
                   .show();
               });
             $('.hide-submit-text', form).remove();
@@ -133,4 +133,4 @@
     }
   };
 
-})(jQuery, Drupal, window, this.document);
+})(jQuery, Backdrop, window, this.document);
